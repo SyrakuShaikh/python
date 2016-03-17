fe9c05dd13f6d65794d19d3a404b1731057de97c
SyrakuShaikh <shkxjtz@gmail.com>
2016-03-17 21:47:01 +0800

    chapter 16 standard library

--- learning/a_byte_of_python/stdlib_logging.py
+++ learning/a_byte_of_python/stdlib_logging.py
@@ -1,2 +1,27 @@
-# Time-stamp: <2016-03-16 Wed 09:05:02 Shaikh>
+# Time-stamp: <2016-03-17 Thu 21:46:22 Shaikh>
 # -*- coding: utf-8 -*-
+import os
+import platform
+import logging
+
+
+if platform.platform().startswith('Windows'):
+    logging_file = os.path.join(os.getenv('HOMEDRIVE'),
+                                os.getenv('HOMEPATH'),
+                                'test.log')
+else:
+    logging_file = os.path.join(os.getenv('HOME'),
+                                'test.log')
+
+print("Logging to", logging_file)
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s : %(levelname)s :%(message)s',
+    filename=logging_file,
+    filemode='w',
+)
+
+logging.debug("start of the program")
+logging.info("Doing something")
+logging.warning("Dying now")
