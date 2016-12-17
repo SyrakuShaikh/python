fa5b52d9f183264d7919b09741ee42211cdbbbc3
SyrakuShaikh <shkxjtz@gmail.com>
2016-12-17 19:28:35 +0800

    python hello.py and ./helllo.py is exactly the same for sys.argv.

---
+++ learning/liaoxuefeng_tutor/sys_args.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Time-stamp: <2016-12-17 Sat 19:28:08 Shaikh>
+"""
+Test 'sys' module's 'argv' method.
+
+Is there any difference between ~python3 hello.py~ and ~./hello.py~.
+"""
+
+import sys
+
+def test():
+    """
+    test function, main function.
+    """
+    args = sys.argv
+    if len(args) == 1:
+        print('Hello, World!')
+    elif len(args) == 2:
+        print('Hello, %s' % args[1])
+    else:
+        print('Too many arguments.')
+
+
+if __name__ == '__main__':
+    test()
