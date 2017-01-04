5f8f35797807de7c0347cb4b88abd3a28192404d
SyrakuShaikh <shkxjtz@gmail.com>
2017-01-04 14:37:24 +0800

    init for Project Euler #4. a medium problem, harder than #169?

---
+++ hackerrank/project_euler/4.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Time-stamp: <2017-01-04 Wed 14:35:33 Shaikh>
+"""
+Project Euler #4:
+
+A palindromic number reads the same both ways. The smallest 6 digit palindrome
+made from the product of two 3-digit numbers is 101101 = 143 x 707.
+
+Find the largest palindrome made from the product of two 3-digit numbers which
+is less than N.
+"""
