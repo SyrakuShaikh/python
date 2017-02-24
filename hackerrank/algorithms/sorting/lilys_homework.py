b500992d329ee957c1e76d2156bebbec17de5db0
SyrakuShaikh <shkxjtz@gmail.com>
2017-02-24 21:22:16 +0800

    rough version of Dual-Pivot Quick Sort with lots of errors.

--- hackerrank/algorithms/sorting/lilys_homework.py
+++ hackerrank/algorithms/sorting/lilys_homework.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# Time-stamp: <2017-02-23 Thu 21:44:33 Shaikh>
+# Time-stamp: <2017-02-24 Fri 21:20:27 Shaikh>
 """
 Lily's Homework
 
@@ -16,9 +16,29 @@ beautiful if the sum of |a_i - a_i-1| among 0 < i < n is minimal possible
 Given the array A, find and print the minimum number of swaps that should be
 performed in order to make the array beautiful.
 """
-def quick_sort(ar: list, st: int, ed: int):
+def insertion_sort(ar: list):
+    """
+    Insertion Sort Function.
+    """
+    for i in range(1, len(ar)):
+        pivot = ar[i]
+        for j in range(i-1, -1, -1):
+            if ar[j] > pivot:
+                ar[j+1] = ar[j]
+            else:
+                ar[j+1] = pivot
+                break
+        else:
+            ar[j] = pivot
+
+
+def cqc(ar: list, st: int, ed: int):
     """
     Quick Sort Function.
+
+    Single pivot version.
+    So-called classic quick sort.
+    Fixed position pivot.
     """
     if ed - st < 2:
         return
@@ -32,8 +52,67 @@ def quick_sort(ar: list, st: int, ed: int):
             index += 1
     if index != ed-1:
         ar[ed-1], ar[index] = ar[index], ar[ed-1]
-    quick_sort(ar, st, index)
-    quick_sort(ar, index+1, ed)
+    cqc(ar, st, index)
+    cqc(ar, index+1, ed)
+
+
+def dpqc(ar: list, left: int, right: int):
+    """
+    Dual-Pivot Quick Sort.
+    """
+    if right - left < 10:
+        print(*ar[left:right+1])
+        cqc(ar, left, right+1)
+        print(*ar[left:right+1])
+        return
+    # record the initial left index which may be used
+    # later when the partition completes.
+    oldleft = left
+    # locate the two pivots' index
+    while ar[left] >= ar[right] and left < right:
+        if ar[left] > ar[right]:
+            ar[left], ar[right] = ar[right], ar[left]
+            break
+        else:
+            left += 1
+
+    pivot1 = ar[left]
+    pivot2 = ar[right]
+
+    less = left + 1
+    while less < right and ar[less] < pivot1:
+        less += 1
+    # when loop ends, ar[less] >= pivot1
+
+    king = less + 1
+    while king < right and pivot1 <= ar[king] <= pivot2:
+        king += 1
+    # when loop ends, ar[king] is < pivot1 or > pivot2.
+
+    great = right - 1
+    while great >= king and ar[great] > pivot2:
+        great -= 1
+    # when loop ends, ar[great] <= pivot2.
+
+    while king <= great:
+        temp = ar[king]
+        if temp <= pivot2:
+            ar[king], ar[less] = ar[less], ar[king]
+            if temp < pivot1:
+                less += 1
+            king += 1
+        else:
+            ar[king], ar[great] = ar[great], ar[king]
+            great -= 1
+    # partition completes!
+    ar[right], ar[great] = ar[great], ar[right]
+    ar = ar[0:oldleft] + ar[left+1:less] + ar[oldleft:left+1] + ar[less:]
+    # recursion
+    print(*ar)
+    dpqc(ar, oldleft, oldleft+less-left-2)
+    dpqc(ar, less, great-1)
+    dpqc(ar, king+1, right)
+
 
 
 def swaps(ar: list, ars: list) -> int:
@@ -57,16 +136,27 @@ def swaps(ar: list, ars: list) -> int:
             ard.pop(a)
     return n
 
-t = int(input().strip())
-array = [int(x) for x in input().strip().split(' ')]
-arrays = array[:]
+# t = int(input().strip())
+# array = [int(x) for x in input().strip().split(' ')]
+# arrays = array[:]
 
 # here used the built-in 'sort' function can pass all the test_cases
 # therefore I need to find a better sort algorithm.
-# (my quick_sort is not good enough)
-quick_sort(arrays, 0, len(arrays))
-arrayss = arrays[:]
-arrayss.reverse()
-c1 = swaps(array, arrays)
-c2 = swaps(array, arrayss)
-print(min(c1, c2))
+# (my cqc is not good enough)
+# cqc(arrays, 0, len(arrays))
+# if t < 47:
+#     insertion_sort(arrays)
+# else:
+#     dpqc(arrays, 0, len(arrays)-1)
+
+# arrayss = arrays[:]
+# arrayss.reverse()
+# c1 = swaps(array, arrays)
+# c2 = swaps(array, arrayss)
+# print(min(c1, c2))
+
+from random import sample
+arr = sample(list(range(1, 200)), k=30)
+print(*arr)
+dpqc(arr, 0, len(arr)-1)
+print(*arr)
