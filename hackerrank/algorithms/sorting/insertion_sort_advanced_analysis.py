a00b49c9f3e733465e50beb2ffd867ee9bd0be12
SyrakuShaikh <shkxjtz@gmail.com>
2017-03-03 20:50:33 +0800

    Still failed.
    I 'invent' a new method to work on this problem. But still, failed. I realized
    that this method cannot work on some lists with special structure.
    
    From the 'discussion', I noted that Binary Indexed Tree or Red-black Tree can be
    applied on this problem. Unfortunately, not learned till now.

--- hackerrank/algorithms/sorting/insertion_sort_advanced_analysis.py
+++ hackerrank/algorithms/sorting/insertion_sort_advanced_analysis.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# Time-stamp: <2017-01-29 Sun 16:15:17 Shaikh>
+# Time-stamp: <2017-03-03 Fri 20:38:41 Shaikh>
 """
 Insertion Sort Advanced Analysis
 
@@ -21,25 +21,108 @@ def sorttest(l: list) -> bool:
             return False
     return True
 
-def larger(l: list) -> int:
+
+def larger(element: int, LIST: list) -> int:
+    """
+    Count the larger elements than the given one.
+    """
+    temp = 0
+    for i in LIST:
+        if i > element:
+            temp += 1
+    return temp
+
+
+def counting(length: int, LIST: list) -> int:
     """
     Calculate the number of elements in a list that larger than the tail
     element.
     """
-    tail = l[-1]
-    num = 0
-    for i in l[:-1]:
-        if i > tail:
-            num += 1
-    return num
+    sums = 0
+    for i in range(1, length):
+        sums += larger(LIST[i], LIST[:i])
+    return sums
+
+
+def divide(element: int, LIST: list) -> list:
+    """
+    Divide one list to two lists.
+
+    This function divides one list to two lists based on whether the elements
+    in it is larger than the given element.
+    """
+    partitions = [[], []]
+    MAX = None # the largest one less than the given element.
+    if len(LIST) == 0:
+        return [[], [element], MAX]
+    for i in LIST:
+        if i > element:
+            partitions[0].append(i)
+        else:
+            if i < element:
+                MAX = i if MAX is None else max(MAX, i)
+            partitions[1].append(i)
+    return partitions + [MAX]
+
+
+def insertion(length: int, LIST: list) -> int:
+    global PARTS
+    sums = 0
+    if length < 50:
+        return counting(length, LIST)
+    else:
+        sums = counting(49, LIST[:49])
+
+        i = 49
+        temp = divide(LIST[i], LIST[:i])
+        PARTS[LIST[i]] = temp[0]
+        PARTS['R'] = temp[1] + [LIST[i]]
+
+        while i < length-1:
+            i += 1
+
+            COMP = list(PARTS.keys())
+            COMP.remove('R')
+            tempa = divide(LIST[i], COMP)
+
+            for j in tempa[0]:
+                sums += len(PARTS[j])
+
+            if LIST[i] in COMP:
+                sums += len(PARTS[LIST[i]])
+                if tempa[2] is None:
+                    PARTS['R'] += [LIST[i]]
+                else:
+                    PARTS[tempa[2]] += [LIST[i]]
+            elif tempa[2] is None:
+                tempb = divide(LIST[i], PARTS['R'])
+                sums += len(tempb[0])
+                PARTS[LIST[i]] = tempb[0]
+                PARTS['R'] = tempb[1] + [LIST[i]]
+            else:
+                tempc = divide(LIST[i], PARTS[tempa[2]])
+                sums += len(tempc[0])
+                if len(PARTS[tempa[2]]) > 1000:
+                    PARTS[LIST[i]] = tempc[0]
+                    PARTS[tempa[2]] = tempc[1] + [LIST[i]]
+
+        return sums
 
 
 t = int(input().strip())
 for i in range(t):
     n = int(input().strip())
     arr = [int(x) for x in input().strip().split(' ')]
-    sums = 0
+    PARTS = dict()
+    total = 0
     if n != 1 and not sorttest(arr):
-        for j in range(1, n):
-            sums += larger(arr[:j+1])
-    print(sums)
+        total = insertion(n, arr)
+    print(total)
+
+# from random import sample
+# PARTS = dict()
+# arr = sample(list(range(1, 200)), k=100)
+# total = 0
+# if len(arr) != 1 and not sorttest(arr):
+#     total = insertion(len(arr), arr)
+# print(total)
