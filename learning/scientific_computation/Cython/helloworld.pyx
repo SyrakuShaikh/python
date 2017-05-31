b51a3b422069b5b575141a480577b0cc2ebc1188
SyrakuShaikh <shkxjtz@gmail.com>
2017-05-31 16:03:37 +0800

    Starter sample, pyximport module.

---
+++ learning/scientific_computation/Cython/helloworld.pyx
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Time-stamp: <2017-05-31 Wed 16:01:35 Shaikh>
+print("Hello, world!")
---
+++ learning/scientific_computation/Cython/helloworld_import.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Time-stamp: <2017-05-31 Wed 16:02:55 Shaikh>
+import pyximport
+pyximport.install()
+import helloworld
